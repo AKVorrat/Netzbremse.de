abe8929fb0a9b7f141d3093c0c7e33545bb1c003
Tobias Muehlberger <tobias@muehlberger.dev>
2025-11-05 22:11:17 +0100

    Fixed chat bubble + allow open more than one faq at a time

--- nb-speedtest/src/FaqItem.tsx
+++ nb-speedtest/src/FaqItem.tsx
@@ -2,7 +2,7 @@ import { ParentComponent } from "solid-js";
 
 export const FaqItem: ParentComponent<{ title: string }> = (props) => {
   return (
-    <details class="collapse collapse-arrow bg-primary" name="faq-item">
+    <details class="collapse collapse-arrow bg-primary">
       <summary class="collapse-title font-semibold font-title text-primary-content text-xl">{props.title}</summary>
       <div class="collapse-content prose !prose-invert text-primary-content">
         {props.children}
